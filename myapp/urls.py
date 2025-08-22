4c7035728cc7c424e62c628db554278a582f5e82
Tanachai <tananachaiii@gmail.com>
2025-08-22 11:59:06 +0700

    edit path index page to All1,All2

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('',views.index)
+    path('', views.index),
+    path('All1', views.All1, name='All1'),
+    path('All2', views.All2, name='All2')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+
+def All1(request):
+    return render(request,"All1.html")
+
+def All2(request):
+    return render(request,"All2.html")
